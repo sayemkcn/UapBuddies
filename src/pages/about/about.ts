8226421ada8cf3bc8037c2ecf15b279858d73a9d
Sayem Hossain <sayemkcn@gmail.com>
2016-12-12 23:51:10 +0600

    About page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -12,4 +12,15 @@ export class AboutPage {
 
   }
 
+  openUrl(urlName : string){
+    switch(urlName){
+      case "facebook":
+        window.open("https://www.facebook.com/SAyEM.RimOn");
+      break;
+      case "playstore":
+        window.open("https://play.google.com/store/apps/dev?id=9183820597138392476");
+      break;
+    }
+  }
+
 }
